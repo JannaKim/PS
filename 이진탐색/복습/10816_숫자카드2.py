fc480e28de716738f468a215feebc15d295b96e7
JannaKim <74404132+JannaKim@users.noreply.github.com>
2020-11-25 21:59:26 +0900

    settin range using BS
    In order to count the number of element in L, left range should aim the leftest element's index, and right range should aim index rightside index to all aimed element.
    But in some case where aimed element doesn't exist,  the left range index should point out index where L[index]<aimed element. In such case, the function should return i+1 (L[i+1]>element) so the returned element will coincide the right range index so the numer of element will correctly be counted as 0.
    In other case where the aimed element is the biggest or bigger then one elements of all L, then
    right range index <=aimed element
    In such case, it should also return i+1. Or not, the element should be counted one less to the correct answer.

--- 이진탐색/복습/10816_숫자카드2.py
+++ 이진탐색/복습/10816_숫자카드2.py
@@ -1,8 +1,11 @@
-import sys
-sys.setrecursionlimit(500000)
+from sys import *
+setrecursionlimit(500000)
+input = lambda: stdin.readline().rstrip()
 N = int(input())
 L = [int(i) for i in input().split()]
-
+M = int(input())
+C = [int(i) for i in input().split()]
+'''
 def merge(L, first, last):
     m = (first+last)//2
     i=first
@@ -19,7 +22,7 @@ def merge(L, first, last):
         A.append(L[k])
     for k in range(j,last+1):
         A.append(L[k])
-    for i in range(first,last):
+    for i in range(first,last+1):
         L[i] = A[first-i]
         
 def ms(L,first,last):
@@ -30,16 +33,53 @@ def ms(L,first,last):
     ms(L,m+1,last)
     merge(L,first,last)
 ms(L,0,N-1)
-print(L)
+'''
+
+def left(L,k): # 첫 k에 가는 게 목표였는데 k가 없다면 더 작은 곳에 가있을것
+
+    i, j = 0,N-1
+    while i<j:
+        m = (i+j)//2
+        if L[m]<k:
+            i=m+1
+        else:
+            j=m
+    if L[i]<k:
+        return i+1
+    return i
+
+def right(L,k): # k앞에 가는 게 목표인데 k가 인덱스의 마지막에 있었다면 k에 가있을것
+    i, j = 0,N-1
+    while i<j:
+        m=(i+j)//2
+        if L[m]<=k:
+            i=m+1
+        else:
+            j=m
+    if L[i]<=k:
+        return i+1
+    return i
+
+L.sort()
+
+ans=[]
+for el in C:
+    a = left(L,el)
+    b = right(L,el)
+    ans.append(b-a)
+print(*ans)
+
+
 
 '''
 10
 6 3 2 10 10 10 -10 -10 7 3
+8
+10 9 -5 2 3 4 5 11
 
-5
-5
+
 2
-3
-4
-1
+1 2
+8  
+0 1 0 1 2 3 4 5   
 '''
