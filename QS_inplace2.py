b0e9254baab72d01831b76d5d7be96a0bd6923a2
JannaKim <74404132+JannaKim@users.noreply.github.com>
2020-11-25 20:26:59 +0900

    mergesort
    when handling the leftovers, make sure index the giver A follws k, not i or j

--- QS_inplace2.py
+++ QS_inplace2.py
@@ -3,26 +3,30 @@ sys.setrecursionlimit(500000)
 N = int(input())
 L = [int(input()) for _ in range(N)]
 
-def qs(L, first, last):
+def merge(L, first, last):
+    m = (first+last)//2
+    i=first
+    j=m+1
+    A=[]
+    while i<m and j<last:
+        if L[i]<=L[j]:
+            A.append(L[i])
+            i+=1
+        else:
+            A.append(L[j])
+            j+=1
+    for k in range(i,m+1):
+        A.append(L[i])
+    for k in range(j,last+1):
+        A.append(L[j])
+    for i in range(first,last):
+        L[i] = L[first,i]
+        
+def ms(L,first,last):
     if first>=last:
         return
-    p = L[(first+last)//2]
-    i = first
-    j = last
-    while i<=j:
-        while L[i]<p:
-            i+=1
-        while L[j]>p:
-            j-=1
-        
-        if i<=j:
-            a, b = L[i], L[j]
-            L[i], L[j] = b, a
-            i+=1
-            j-=1
-    qs(L,first,i-1)
-    qs(L,i,last)
-
-
-qs(L, 0,N-1)
-[print(i) for i in L]
+    m=(first+last)//2
+    ms(L,first,m)
+    ms(L,m+1,last)
+    merge(L,first,last)
+print(L)
