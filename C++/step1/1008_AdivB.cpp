207c91ad84d41ce6ae08c34d9b9150e295fd3fdf
Jannakim <baradamoh@gmail.com>
2020-12-03 19:11:56 +0900

    deciding num of significant digits
    #include <ios> // define type streamsize
    #include <iomanip> // define setprecision
    
    streamsize prec = cout.precision(); // store initial value
    fixed
    
    1. as a whole
    cout<<setprecision(4)<<3.14159<<setprecision(prec)<<'\n'; // 3.1416 and remove setting
    
    2. only the floating points
    cout<<fixed<<setprecision(5)<<3.14<<setprecision(prec)<<'\n'; // 3.14000 and initialize.

--- C++/step1/1008_AdivB.cpp
+++ C++/step1/1008_AdivB.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <string>
+
+#include <iomanip>
+#include <ios>
 using namespace std;
 int main()
 {
@@ -9,12 +13,41 @@ int main()
     //printf("%.9f\n",A/B);
     //printf: 실수, 소수자리수를 각각 지정할 수 있다
     //cout: 실수와 소수를 초함한 자리수를 지정한다
-    cout<<fixed; // 소수점을 고정시켜 표현
-    cout.precision(9);
-    cout<<A/B<<endl;
-    cout.unsetf(ios::fixed);
+
+
+    // cout.setf(ios::fixed);
+
     //cout<<A*10/B<<endl;
 
+
+    //총 세 자리로 출력하기
+
+    /*
+
+    endl 느리다: 줄바꿈을 출력하고 flush하는것까지 포함하기 때문.
+
+
+    출력속도는 cin cout이 scanf, printf 보다 느리다.
+
+    ios_base::sync_with_stdio(false); //하면 매우빨라지지만 printf와 섞어서 쓸 수 없다
+    */
+
+    streamsize prec = cout.precision();
+    cout<<3.14159265<<'\n' // 3.14159265
+    <<setprecision(3)<<3.14159265<<'\n' //3.14
+    <<setprecision(prec)<<3.14159265<<'\n' //3.14159265
+    <<fixed<<setprecision(9)<<3.14159265<<'\n' //3.141592650
+    <<setprecision(prec)<<3.141592653589 <<'\n'//3.141593
+    <<fixed<<setprecision(4)<<3.14159265<<'\n' //3.1416
+    <<setprecision(prec)<<3.141592653589 <<'\n';//3.141593
+
+
+    /*
+    string s;
+    getline(cin,s);
+
+    cout<<s<<endl;
+    */
 
 
 }
--- C++/step1/10869_4fundamentalarithmeticop.cpp
+++ C++/step1/10869_4fundamentalarithmeticop.cpp
@@ -4,6 +4,7 @@ int main(){
     int A, B;
     cin>>A;
     cin>>B;
+    //scanf("%d %d", &a, &b);
     int quotient = (A+B)>>1;//  비트를 왼쪽에서 오른쪽으로 1비트 이동할 때마다 2로 나누는 효과
 
 
