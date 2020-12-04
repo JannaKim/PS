25da22fec5f6be9e4f5f42e72c6bcbf27e8c21d3
Jannakim <baradamoh@gmail.com>
2020-12-05 00:19:27 +0900

    char 2 , reference variable 4
    char: 0~60000 2byte.
    short: -30000~30000
    all reference variables are initialized as null
    
    char = ' '; //error. space is not ASCII
    
    public static void main(String[] arv)
    static public void main(String[] args)
    
    bw.flush();
    bw.close();

---
+++ Java/Chap2Exercises.java
@@ -0,0 +1,29 @@
+package Java;
+import java.io.*;
+import java.util.*;
+public class Chap2Exercises 
+{
+    static public void main(String arv[]) throws Exception
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        //9507092048216 1조 : long
+        long regNo;
+        String s= br.readLine(); 
+        regNo = Long.parseLong(s);
+
+        bw.write(s+"\n",1,5); //offset위치부터 length크기만큼 씀
+
+   
+        //System.out.println('1'+0); // '1': 49
+        //System.out.println(true);
+        //int system = 3;
+        bw.write(3+"\n");
+        bw.flush();
+
+        bw.close();
+
+    }
+    
+}
