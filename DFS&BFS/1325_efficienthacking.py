75ea4cbee2d80f145a1567bc6eff7ed4f1ccf438
Jannakim <baradamoh@gmail.com>
2020-12-05 02:44:07 +0900

    runtime error

---
+++ DFS&BFS/1325_efficienthacking.py
@@ -0,0 +1,44 @@
+import sys; setrecursionlimit(1000000)
+raw_input = lambda: sys.stdin.readline()
+N, M = map(int, raw_input().split())
+edge = [[] for _ in range(N+1)]
+indegree = [0]*(N+1)
+for _ in range(M):
+    a, b = map(int, raw_input().split())
+    edge[b].append(a)
+    indegree[a]+=1
+
+check = [False]*(N+1) # uninitialize visited array?
+def dfs(v):
+    global cnt
+    for v2 in edge[v]:
+        if not check[v2]:
+            cnt+=1
+            check[v2]=True
+            dfs(v2)
+
+
+
+cnt=1
+ans=[]
+mx=-1
+for i in range(1,N+1):
+    if not indegree[i]:
+        dfs(i)
+        mx = max(mx, cnt)
+        ans.append((i, cnt))
+        check = [False]*(N+1)
+        cnt=1
+A = []
+[A.append(str(el[0])) for el in ans if el[1]==mx]
+print(" ".join(A))
+        
+
+
+'''
+5 4
+3 1
+3 2
+4 3
+5 3
+'''
