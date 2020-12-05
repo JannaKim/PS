9ee36722123974dedf5141379553e76ab01c52c0
JannaKim <74404132+JannaKim@users.noreply.github.com>
2020-12-05 15:38:20 +0900

    renewing max elements, saving memory
    1. to make list ans consisting only with valid one, ititiallize ans = [i] when new max val is found, else appand value when it matchs the current value.
    
    2. setrecursionlimit  requires memory

--- DFS&BFS/1325_efficienthacking.py
+++ DFS&BFS/1325_efficienthacking.py
@@ -1,37 +1,55 @@
-import sys; sys.setrecursionlimit(1000000)
-input = lambda: sys.stdin.readline()
+import sys; 
+input = lambda: sys.stdin.readline().rstrip()
+from collections import deque
 N, M = map(int, input().split())
 edge = [[] for _ in range(N+1)]
-indegree = [0]*(N+1)
+#indegree = [0]*(N+1)
 for _ in range(M):
     a, b = map(int, input().split())
     edge[b].append(a)
-    indegree[a]+=1
+    #indegree[a]+=1
 
-check = [False]*(N+1) # uninitialize visited array?
+#check = [False]*(N+1) # uninitialize visited array?
 def dfs(v):
-    global cnt
-    for v2 in edge[v]:
-        if not check[v2]:
-            cnt+=1
-            check[v2]=True
-            dfs(v2)
+    check = [False]*(N+1)
+    check[v]=True
+    cnt = 1
+    q = deque()
+    q.append(v)
+    while q:
+        v1 = q.popleft()
+        for v2 in edge[v1]:
+            if not check[v2]:
+                cnt+=1
+                check[v2]=True
+                q.append(v2)
+    return cnt
 
 
 
-cnt=1
 ans=[]
-mx=-1
+mx=0
+
 for i in range(1,N+1):
-    if not indegree[i]:
-        dfs(i)
+    #if not indegree[i]:
+    if edge[i]:
+        cnt = dfs(i)
+        '''
         mx = max(mx, cnt)
-        ans.append((i, cnt))
-        check = [False]*(N+1)
-        cnt=1
+        ans.append((i, cnt)) #메모리 초과
+        '''
+        if cnt>mx: # 새로운 mx찾으면 답 리스트 초기화
+            ans=[i]
+            mx=cnt
+        elif cnt==mx: # 새로운 mx와 같은 애면 리스트 추가
+            ans.append(i)
+'''       
 A = []
 [A.append(str(el[0])) for el in ans if el[1]==mx]
 print(" ".join(A))
+'''
+
+print(*ans)
         
 
 
