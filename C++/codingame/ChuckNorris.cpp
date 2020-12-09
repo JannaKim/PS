d166a7632905954f7e842f34b1559153a7f10a53
Jannakim <baradamoh@gmail.com>
2020-12-10 01:54:00 +0900

    how? string raw_bin="       ";
    string raw_bin=" "*7;   ?
    
    AC

--- C++/codingame/ChuckNorris.cpp
+++ C++/codingame/ChuckNorris.cpp
@@ -13,24 +13,26 @@ int main()
     for(int j=0; input[j]; j++)
     {
         int mask;
-        //char bin_chunk[6+1]="";
-        string raw_bin="      ";
+        string raw_bin="       "; //자릿수 *로 표현?
         for(int i=6; i>-1; i--)
         {
             mask=1<<i;
             raw_bin[6-i] = input[j]&mask?'1':'0';
         }
-        //printf("%s\n",bin_code);
+        /*
         int dest=0;
         while(raw_bin[dest]!='1') dest++;
 
-        bin_code += string(raw_bin.substr(dest,7-dest));
-    }
-    cout<<bin_code<<'\n';
+        bin_code += string(raw_bin.substr(dest));
+        */
+        bin_code += raw_bin;
 
-    for(int i=0; i<bin_code.length();)
+    }
+    //cout<<bin_code<<'\n';
+    int binLen=bin_code.length();
+
+    for(int i=0; i<binLen;)
     {
-        //printf("cur= %c\n",bin_code[i]);
         int cnt=0;
         if(bin_code[i]=='0'){
             
@@ -41,7 +43,7 @@ int main()
             while(bin_code[i]=='1') {cnt++; i++;}
             cout<<string("0 ")<<string(cnt,'0');
         }
-        if(i!=7) cout<<" ";
+        if(i!=binLen) cout<<" ";
     }
 
 
