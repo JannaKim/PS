ad9454c5123d4633f0d2651ba10abded2f5b2ebf
Jannakim <baradamoh@gmail.com>
2020-12-12 22:35:43 +0900

    Static member & singleton
    1. Static member:
    cannot be created as an instance.
    call and modify static member elements by
    classname.(field or mathod)
    
    2. singleton?
    creates instance of such class once and only.
    Its one and only instance is defined INSIDE the Singleton class.
    
    private static Singleton singleton = new Singleton();
    
    2.
    state
    private Singleton() {},
    so instance CANT be created outside.
    
    * Why not just define Singleton class as static? -> then it cant be called; its unusable... we should state class Singletone as public and make a static method so it can be callable outside.
    
    3. like this:
    static Classname getInstance(){
     return oneAndOnlyInstance;
    }
    and its address will be returned.

---
+++ CLASS/SingletonEx.java
@@ -0,0 +1,20 @@
+package CLASS;
+
+public class SingletonEx {
+    public static void main(Strings[] arv)
+    {
+
+
+    }
+}
+
+
+
+
+public class Singleton { //객체가 하나만 만들어지도록
+    private static Singleton singleton = new Singleton();
+
+    private Singleton() {}
+
+    
+}
