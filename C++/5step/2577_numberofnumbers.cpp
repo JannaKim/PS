fa9e0fe89d14e3aed16639d75686c1d1a1129658
Jannakim <baradamoh@gmail.com>
2020-12-19 00:49:43 +0900

    form of web service
    static web page->
    
    dynamic: request-base HTML from CGI+db
    
    minimize code revision: MVC pattern.
    divide to 3 pieces,
    and mend the visible ones only.
    
    speed?:
    Rest API: 3 computers charges different role individually.

---
+++ C++/5step/2577_numberofnumbers.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <vector>
+#include <string>
+#include <stdlib.h>
+using namespace std;
+int main(){ //컴파일 언어: 시작점을 갖고있다.
+//컴파일 언어는 텍스트 형태의 소스코드를 컴파일해 기계어 형태로 된 실행파일(binary)을 만들어낸다.
+//실행파일은 CPU에서 바로 싱행되지 깨문에 속도가 빠르고 간결하다.
+int a, b, c;
+scanf("%d\n%d\n%d",&a,&b,&c);
+long X = (long)a*b*c;
+
+string s= to_string(X);
+printf("%s\n",s.c_str());
+int info[10]= {0, };
+for(int i=0;i<s.length();i++)
+    info[s[i]-'0']+=1;
+
+for(int i=0;i<10;i++)
+    printf("%d\n",info[i]);
+
+}
