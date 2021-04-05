db263829fe30a6f4d1950829bdd6846fb9fb22e3
Jannakim <baradamoh@gmail.com>
2021-04-06 02:55:46 +0900

    sudoku solved
    1) answer printing should be focused in case all board are filled.
    2) If passed to next candidate, put it back to zero to inform func that ths spot should be calculated again.

--- contest/algostudy/2.py
+++ contest/algostudy/2.py
@@ -1,10 +1,8 @@
 import sys; input= lambda: sys.stdin.readline().rstrip()
 
 def coinChange(coins, amount):
-    amount=int(amount) # 혹시 몰라서 넣었어요
     dp= [0]+[1e9]*amount
     for won in coins:
-        won= int(won) # 혹시 몰라서
         for i in range(amount+1):
             if (i==0 or dp[i]<1e9) and i+won<=amount:
                 dp[i+won]=min(dp[i+won], dp[i]+1)
--- recursion/2580_스도쿠.py
+++ recursion/2580_스도쿠.py
@@ -1,15 +1,11 @@
 import sys; input= lambda: sys.stdin.readline().rstrip()
-
 mp= [[*map(int, input().split())] for _ in range(9)]
 
-
 def fill(i, j):
-    if (i, j)==(9, 0):
-        [print(*el) for el in mp]
-        exit()
-
+    found= False
     while i<=8 and j<=8:
         if not mp[i][j]:
+            found=True
             chk= [True]+[False]*9
             for k in range(9):
                 chk[mp[i][k]]=True
@@ -20,21 +16,32 @@ def fill(i, j):
             for y in range(a, a+3):
                 for x in range(b, b+3):
                     chk[mp[y][x]]=True
-            #print(R, C, B, sep='\n')
             for t in range(1,10):
                 if not chk[t]:
                     mp[i][j]=t
-                    #[print(*el) for el in mp]
-                    #print()
                     nxt_j= j+1
                     nxt_i= i+(j+1)//9
                     nxt_j%=9
                     fill(nxt_i, nxt_j)
-                    mp[i][j]=0
+                    mp[i][j]=0 # 이거 안해주면 갈아엎기 안됨
             break
         j+=1
         i+=j//9
         j%=9
+    if not found: # (9,0) 까지 도달 못해면 다 채웠어도 출력안함
+        [print(*el) for el in mp]
+        exit()
+
 
 fill(0,0)
-
+'''
+1 3 5 4 6 9 2 7 8
+7 0 2 1 0 5 6 0 9
+4 6 9 2 7 8 1 3 5
+3 2 1 5 4 6 8 9 7
+8 0 4 9 0 3 5 0 6
+5 9 6 8 2 7 4 1 3
+9 1 7 6 5 2 3 8 4
+6 0 3 7 0 1 0 0 0
+2 5 8 3 9 4 0 0 0
+'''
